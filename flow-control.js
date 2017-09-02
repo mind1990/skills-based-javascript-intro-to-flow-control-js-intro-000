c692518083ff914e07c05894698d26ca1a0c80c5
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-02 02:18:32 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -5,11 +5,13 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
 }
 
 function ageChecker(age) {
-
+  
 }
 
 function ternaryTeenager(age) {
